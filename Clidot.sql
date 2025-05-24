c37d052a67801ac1b141e12422ab13fd90237753
Ravi1983-dev <ravishankar.sg2@gmail.com>
2025-05-24 15:06:22 +0530

    Update Clidot.sql

--- Clidot.sql
+++ Clidot.sql
@@ -1,5 +1,6 @@
 --Create the Login and then map to the Clidot_Prod database
 
+USE master
 -- Step 1: Create server login
 IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'Clidot_Prod_isource')
 BEGIN
@@ -7,13 +8,13 @@ BEGIN
 END
 
 -- Step 2: Create user in specific database (change context)
-USE Clidot_Prod;
+USE Clidot_Prod
 GO
 
 -- Step 3: Create DB user and map to login
 IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Clidot_Prod_isource')
 BEGIN
-    CREATE USER Clidot_isource FOR LOGIN Clidot_isource;
+    CREATE USER Clidot_Prod_isource FOR LOGIN Clidot_Prod_isource;
     EXEC sp_addrolemember 'db_datareader', 'Clidot_Prod_isource';
     EXEC sp_addrolemember 'db_datawriter', 'Clidot_Prod_isource';
 END
